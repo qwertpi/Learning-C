b0b7797814d71ef145a4dfbe12241ad23cbd9921
qwertpi <roarser2004@gmail.com>
2018-07-24 15:24:48 +0100

    Added best score saving functionality

--- number guessing game/game.c
+++ number guessing game/game.c
@@ -2,15 +2,48 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
+int read (char mode[10])
+{
+  // declare a pointer called fp of the type file
+  FILE *fp;
+  int number;
+  // turn fp into a pointer to file.txt in read binary mode
+  fp = fopen (strcat (mode, ".txt"), "rb");
+// fp will be 0 ie true if the file exists
+  while (!feof (fp))
+    {  
+      fscanf (fp, "%d", &number);      
+    }
+    // close the file
+    fclose (fp);
+  return number;
+}
+
+int write(int value, char mode[10])
+{
+  FILE *fp;
+  char str[10];
+  fp = fopen (mode, "wb");
+  sprintf(str, "%d", value);
+  fprintf (fp, str);
+  fclose (fp);
+}
+
 int main(void) 
 {
   int random_number;
   int guess;
   int guesses=0;
+  char strmode[10];
   int mode;
+  int best_guess;
   printf("Mode 1: 0-50\nMode 2: 0-100\nMode 3: 0-250\nMode 4: 0-500\nMode 5 0-2000\n1-5?");
   scanf("%d",&mode);
-  srand(time(NULL));
+  sprintf(strmode, "%d", mode);
+  best_guess=read(strmode);
+srand(time(NULL));
+
   switch(mode)
   {
     case 1: random_number=round(rand()/(RAND_MAX / 50));
@@ -39,5 +72,26 @@ int main(void)
     guesses++;
   }
   printf("It took you %d guesses to get the correct answer of %d\n",guesses,random_number);
+  if (best_guess!=100)
+  {
+if (guesses<best_guess)
+  {
+    printf("Well done, you beat the best score of %d\n",best_guess);
+    write(guesses,strmode);
+  }
+  else if (guesses==best_guess)
+  {
+    printf("Well done, you equaled the best score of %d\n",best_guess);
+  }
+  else
+  {
+    printf("You didn't beat the best score of %d\n",best_guess);
+  }
+  }
+  else 
+  {
+    write(guesses,strmode);
+  }
+  
   return 0;
 }
